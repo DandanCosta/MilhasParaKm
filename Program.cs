23c9cd071da3e97266be260b0521aad924bf14b6
Danilo Costa <dandan_costa2222@outlook.com>
2025-03-27 20:15:56 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+const float Fator = 1.609f;
+double valorMilhas = 0.0, valoremKm = 0.0;
+
+Console.Write("Insira a medida em Milhas: ");
+valorMilhas = double.Parse(Console.ReadLine()!);
+
+valoremKm = valorMilhas * (double)Fator;
+
+Console.Write(valoremKm.ToString("0.000 km"));
